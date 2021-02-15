85135c051579c657a1e47ed4f0127405c6704bfb
Miyamoto Subaru <44694131+FuckingCodeGenerator@users.noreply.github.com>
2021-02-15 17:16:12 +0900

    Update StatusEditor.php

--- StatusEditor/src/metowa1227/StatusEditor.php
+++ StatusEditor/src/metowa1227/StatusEditor.php
@@ -1,41 +1,5 @@
 <?php
 
-/*
-*  __  __       _                             __    ___    ___   _______
-* |  \/  | ___ | |_  ___   _    _  ____  _   |  |  / _ \  / _ \ |___   /
-* | |\/| |/ _ \| __|/ _ \ | |  | |/  _ \/ /  |  | |_// / |_// /    /  /
-* | |  | |  __/| |_| (_) || |__| || (_)   |  |  |   / /_   / /_   /  /
-* |_|  |_|\___| \__|\___/ |__/\__||____/\_\  |__|  /____| /____| /__/
-*
-* All this program is made by hand of metowa1227.
-* I certify here that all authorities are in metowa1227.
-* Expiration date of certification: unlimited
-* Secondary distribution etc are prohibited.
-* The update is also done by the developer.
-* This plugin is a developer API plugin to make it easier to write code.
-* When using this plug-in, be sure to specify it somewhere.
-* Warning if violation is confirmed.
-*
-* Developer: metowa1227
-*/
-
-/*
-    Plugin description
-
-    - CONTENTS
-        - Server status editor
-
-    - AUTHOR
-        - metowa1227
-
-    - DEVELOPMENT ENVIRONMENT
-        - Windows 10 Home 64bit
-        - Intel(R) Core(TM) i7 6700 @ 3.40GHz
-        - 16.00GB DDR4 SDRAM
-        - PocketMine-MP 3.2.2
-        - PHP 7.2.1 64bit supported version
-*/
-
 namespace metowa1227;
 
 use pocketmine\plugin\PluginBase;
